c15402bd8ca2c99d6c7a7fc9709efcb62a4297ce
six-standard <dbrrl1127@gmail.com>
2024-01-03 22:20:25 +0900

    feat :: 헤더 추가

---
+++ components/header.tsx
@@ -0,0 +1,36 @@
+// "use client";
+import { deleteCookie } from "cookies-next";
+import useUserInfo from "@/hooks/useUserInfo";
+import Link from "next/link";
+
+export const Header = () => {
+  const { info, delInfo } = useUserInfo();
+
+  const handleLogout = () => {
+    deleteCookie("accessToken");
+    deleteCookie("name");
+    delInfo();
+  };
+
+  return (
+    <div className="flex justify-between items-center gap-5 pl-10 pr-10 w-full h-10 bg-gray-300">
+      <div className="flex items-center gap-3">
+        <Link href="/">Home</Link>
+        {info.token && <Link href="/write">Write</Link>}
+        <input
+          placeholder="Search"
+          className="w-[3.8rem] h-5 rounded-md p-2 border-spacing-0 outline-0 text-sm transition-all ease-in-out duration-300 focus:w-60 placeholder:text-sm"
+        />
+      </div>
+      {info.token ? (
+        <div className="flex items-center">
+          <h1>{info.name}</h1>
+          <h1>┃</h1>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <Link href="/login">Login</Link>
+      )}
+    </div>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { getCookie } from "cookies-next";
+import { Header } from "@/components/header";
+import useUserInfo from "@/hooks/useUserInfo";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(new QueryClient());
+  const { setInfo } = useUserInfo();
+
+  useEffect(() => {
+    setInfo({
+      token: getCookie("accessToken"),
+      name: getCookie("name"),
+    });
+  }, []);
+
+  return (
+    <div className="grid grid-rows-[1fr_98fr_1fr] gap-5 h-screen">
+      <Header />
+      <div>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </div>
+      <div className="flex pl-10 pr-10 items-center w-full h-10 bg-gray-300">
+        Made with NextJS
+      </div>
+    </div>
+  );
 }
